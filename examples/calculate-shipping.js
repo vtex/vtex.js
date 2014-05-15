7e072fcbb48821194f2cd2b5dea119ea69c05c85
Guilherme Berger <guilherme.berger@gmail.com>
2014-05-15 12:33:04 -0300

    update example

--- examples/calculate-shipping.js
+++ examples/calculate-shipping.js
@@ -1,11 +1,16 @@
-var address = {postalCode: $('#cep').val(), country: 'Brazil'};
-vtexjs.checkout.getAddressInformation(address)
-    .then(function(completeAddress){
-        vtexjs.checkout.calculateShipping(completeAddress);
-    });
+vtexjs.checkout.getOrderForm()
+.then(function(orderForm){
+  var address = {postalCode: $('#cep').val(), country: 'Brazil'};
+  return vtexjs.checkout.getAddressInformation(address)
+})
+.done(function(completeAddress){
+  vtexjs.checkout.calculateShipping(completeAddress);
+});
+
 
 //----------------------------
 
+
 $(window).on('vtexjs.checkout.orderform.update', function(orderForm){
-    meuMinicart.update(orderForm);
+  meuMinicart.update(orderForm);
 });
